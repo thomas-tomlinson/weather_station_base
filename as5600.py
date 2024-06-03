1b1b0ad1a5d6c0ebb0b671109f5b0a1b6417825c
Thomas Tomlinson <thomas_tomlinson@mac.com>
2024-06-03 15:31:10 -0600

    as5600 class

---
+++ as5600.py
@@ -0,0 +1,25 @@
+from micropython import const
+
+AS5600_ADDRESS = const(0x36)   # AS5600 has a fixed address (so can only use one per I2C bus?)
+ANGLE_H	= const(0x0E)          # Angle register (high byte)
+ANGLE_L	= const(0x0F)          # Angle register (low byte)
+
+class AS5600:
+    def __init__(self, i2c=None):
+
+        if i2c is None:
+            raise ValueError('An I2C object is required.')
+        self.i2c = i2c
+        # set power mode to lowest
+        conf = b'\x00\x03'
+        self.i2c.writeto_mem(AS5600_ADDRESS, 0x07, conf)
+
+    def getnReg(self, reg, n):
+        self.i2c.writeto(AS5600_ADDRESS, bytearray([reg]))
+        t =	self.i2c.readfrom(AS5600_ADDRESS, n)
+        return t
+
+    # main function to get angle information
+    def getAngle(self):
+        buf = self.getnReg(ANGLE_H, 2)
+        return ((buf[0]<<8) | buf[1])/ 4096.0*360
