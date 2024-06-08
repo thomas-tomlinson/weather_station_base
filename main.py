86b6730d22628db9fbb240c6d07ef2830fb35243
thomas-tomlinson <thomas_tomlinson@mac.com>
2024-06-08 14:29:48 -0600

    Feature/packed data transfer (#2)
    * vscode pylance setup for micropython
    
    * packed weather data format works

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import bme280_float as bme280
 import json
 import time
 import binascii
+from struct import pack
 from machine import I2C, Pin, UART, lightsleep, RTC, ADC
 from micropython import const
 from ulp_weather import ULP_WEATHER
@@ -23,7 +24,7 @@ rtc = RTC()
 
 def read_bme():
     dict = {}
-    rawread = bme.values
+    rawread = bme.read_compensated_data()
     dict['temp'] = rawread[0]
     dict['pressure'] = rawread[1]
     dict['humidity'] = rawread[2]
@@ -34,15 +35,30 @@ def read_battery():
     # the actual voltage in micro volts.  
     batpin = ADC(Pin(34), atten=ADC.ATTN_11DB)
     try:
-        value = str(float((batpin.read_uv() / 1000000) * 2)) + "V"
+        value = float((batpin.read_uv() / 1000000) * 2)
     except:
-        value = '0.0V'
+        value = 0.0
     return value
 
+def pack_data(payload):
+    packed = pack(">LfHHfffffs", 
+                  payload['timemark'], # L
+                  payload['battery'], # f
+                  payload['rainbuckets'], # H
+                  payload['wind']['wind_dir'], # H
+                  payload['wind']['avg_wind'], # f
+                  payload['wind']['gust_wind'], # f
+                  payload['bme280']['temp'], # f
+                  payload['bme280']['humidity'], # f
+                  payload['bme280']['pressure'], # f
+                  b'\n', #terminate character
+                  ) 
+    return packed
+
 def broadcast_data(payload):
-    transmitPayload = binascii.b2a_base64(payload.encode())
+    #transmitPayload = binascii.b2a_base64(payload.encode())
     # console dump for anyone looking
-    print(transmitPayload)
+    print(payload)
     # wake up HC-12
     set_pin = Pin(23, Pin.OUT)
     set_pin.off()
@@ -53,7 +69,7 @@ def broadcast_data(payload):
     set_pin.on()
     time.sleep_ms(200)
 
-    uart2.write(transmitPayload)
+    uart2.write(payload)
     uart2.flush()
     time.sleep_ms(200)
 
@@ -66,7 +82,6 @@ def broadcast_data(payload):
     # HC-12 now in sleep mode
 
 def init_hc12():
-    # console dump for anyone looking
     # wake up HC-12
     set_pin = Pin(23, Pin.OUT)
     set_pin.off()
@@ -94,22 +109,18 @@ def gather_loop():
     sleep_seconds = 20
     while True:
         payload = {}
-        # BME280 data gather
         payload['bme280'] = read_bme()
         payload['battery'] = read_battery()
         payload['wind'] = read_wind(sleep_seconds)
         payload['rainbuckets'] = read_rain()
 
-        # timestamp
         # we're using seconds since boot as a way to tell the data packets apart.
         payload['timemark'] = time.time()
-        payloadjson = json.dumps(payload)
-        broadcast_data(payloadjson)
+        print(payload)
+        packed_data = pack_data(payload)
+        broadcast_data(packed_data)
 
-        # sleep for 30 seconds
-        #deepsleep(20000)
         lightsleep(sleep_seconds * 1000)
-        #time.sleep(5)
 
 def main():
     init_hc12()
