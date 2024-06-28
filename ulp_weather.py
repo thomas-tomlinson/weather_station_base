44d2d4154563140943e71943cc49c02fc072240b
thomas-tomlinson <thomas_tomlinson@mac.com>
2024-06-28 13:51:05 -0600

    Feature/ulp sanity (#3)
    * this sort of works.  need to remove pin reference from the cpu and init RTC mode in the assembly ULP code
    
    * changes for ulp processing

--- ulp_weather.py
+++ ulp_weather.py
@@ -1,207 +1,118 @@
 from esp32 import ULP
-from machine import mem32, Pin
+from machine import mem32
 import time
-from esp32_ulp import src_to_binary
 
-source = """\
-#define DR_REG_RTCIO_BASE            0x3ff48400
-#define RTC_IO_TOUCH_PAD0_REG        (DR_REG_RTCIO_BASE + 0x94)
-#define RTC_IO_TOUCH_PAD0_MUX_SEL_M  (BIT(19))
-#define RTC_IO_TOUCH_PAD0_FUN_IE_M   (BIT(13))
-#define RTC_GPIO_IN_REG              (DR_REG_RTCIO_BASE + 0x24)
-#define RTC_GPIO_IN_NEXT_S           14
-.set wind_pin, 9  # GPIO32 / RTC_GPIO9 wind anonmeter pin
-.set rain_pin, 5  # GPIO35 RTC_GPIO5, rain bucket pin
-.set loops_per_sec, 200
-
-wind_status:
-        .long 0
-
-wind_status_next:
-        .long 0
-
-rain_status:
-        .long 0
-
-rain_status_next:
-        .long 0
-
-        .global wind_counter
-wind_counter:
-        .long 0
-
-        .global wind_gust
-wind_gust:
-        .long 0
-
-wind_holder:
-        .long 0
-
-        .global rain_counter
-rain_counter:
-        .long 0
-
-        .text
-
-incr_wind_counter:
-        move r3, wind_counter
-        ld r2, r3, 0
-        add r2, r2, 1
-        st r2, r3, 0
-        # increment the wind_holder
-        move r3, wind_holder
-        ld r2, r3, 0
-        add r2, r2, 1
-        st r2, r3, 0
-        #check if wind_holder is more than wind_gust
-        move r3, wind_gust
-        ld r1, r3, 0
-        add r1, r1, 1
-        sub r1, r1, r2
-        jump update_gust, eq 
-        jump rain_check
-
-update_gust:
-        move r3, wind_gust
-        #r2 is the current wind_holder
-        st r2, r3, 0        
-        jump rain_check
-
-wind_status_changed:
-        move r3, wind_status_next
-        ld r2, r3, 0
-        add r2, r2, 1
-        and r2, r2, 1
-        st r2, r3, 0
-
-        jump incr_wind_counter, eq
-        
-        jump rain_check
-
-rain_status_changed:
-        move r3, rain_status_next
-        ld r2, r3, 0
-        add r2, r2, 1
-        and r2, r2, 1
-        st r2, r3, 0
-
-        jump incr_rain_counter, eq
-        jump loop_incr
-
-incr_rain_counter:
-        move r3, rain_counter
-        ld r2, r3, 0
-        add r2, r2, 1
-        st r2, r3, 0
-        jump loop_incr
-
-reset_counter:
-        # reset stage to 0
-        STAGE_RST
-        # reset wind_holder to 0
-        move r3, wind_holder
-        move r1, 0
-        st r1, r3, 0
-
-        halt
-
-
-    .global entry
-entry:
-        READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S, 16)
-        move r1, r0
-
-        # set wind_status, RTC4, GPIO34
-        move r3, wind_status
-        rsh r0, r1, wind_pin
-        and r0, r0, 1
-        st r0, r3, 0
-
-        # set rain_status, RTC5, GPIO35
-        move r3, rain_status
-        rsh r0, r1, rain_pin
-        and r0, r0, 1
-        st r0, r3, 0
-
-        # see if wind_status is changed
-        rsh r0, r1, wind_pin
-        and r0, r0, 1
-        move r3, wind_status_next
-        ld r3, r3, 0
-        add r3, r0, r3
-        and r3, r3, 1
-        jump wind_status_changed, eq        
-
-rain_check:
-        # have to ensure that r1 has the pin status register
-        READ_RTC_REG(RTC_GPIO_IN_REG, RTC_GPIO_IN_NEXT_S, 16)
-        move r1, r0
-        # see if rain_status is changed
-        rsh r0, r1, rain_pin
-        and r0, r0, 1
-        move r3, rain_status_next
-        ld r3, r3, 0
-        add r3, r0, r3
-        and r3, r3, 1
-        jump rain_status_changed, eq        
-
-loop_incr:
-        # increment loop_counter        
-        STAGE_INC 1
-
-        JUMPS reset_counter, 200, EQ
-        halt
-"""
 ULP_MEM_BASE = 0x50000000
 ULP_DATA_MASK = 0xffff  # ULP data is only in lower 16 bits
 
 class ULP_WEATHER:
     def __init__(self):
-        self.binary = src_to_binary(source, cpu="esp32")  
+        f = open('ulp_two_pins.ulp', 'rb')
+        binary = f.read()
+        f.close()
+        
         self.load_addr = 0
+        self.entry_addr = ULP_MEM_BASE + (16*4)
+        self.sleep_micro_seconds = 5000
+
+        # rain 24 hours running total
+        self.init_rain_holder()
+        self.rain_buckets_last_24hours = 0
+        #wind vars in ulp 
         # these values are calculated upon a compile of the ULP code.  Currently
         # this is found through watching the console during the compile and noting the 
         # memory offsets.
-        self.entry_addr = ULP_MEM_BASE + (49*4)
-        self.wind_counter = ULP_MEM_BASE + (4*4)
-        self.wind_gust = ULP_MEM_BASE + (5*4)
-        self.rain_counter = ULP_MEM_BASE +(7*4)
-        
-        self.wind_pin = Pin(32, Pin.IN, pull=None, hold=True)
-        self.rain_pin = Pin(35, Pin.IN, hold=True) # pull down doesn't exist on GPIO35
+        self.ulp_wind_debounce_counter = ULP_MEM_BASE + (2*4)
+        self.ulp_wind_debounce_max_count = ULP_MEM_BASE + (3*4)
+        self.ulp_wind_pulse_edge = ULP_MEM_BASE 
+        self.ulp_wind_next_edge = ULP_MEM_BASE + (1*4)
+        self.ulp_wind_edge_count = ULP_MEM_BASE + (7*4)
+        self.ulp_wind_pulse_min = ULP_MEM_BASE + (6*4)
 
-        ulp = ULP()
-        ulp.set_wakeup_period(0, 5000)  # use timer0, wakeup after 50.000 cycles
-        ulp.load_binary(self.load_addr, self.binary)
+        #rain vars in ulp 
+        self.ulp_rain_debounce_counter = ULP_MEM_BASE + (10*4)
+        self.ulp_rain_debounce_max_count = ULP_MEM_BASE + (11*4)
+        self.ulp_rain_pulse_edge = ULP_MEM_BASE + (8*4) 
+        self.ulp_rain_next_edge = ULP_MEM_BASE + (9*4)
+        self.ulp_rain_edge_count = ULP_MEM_BASE + (15*4)
+        self.ulp_rain_pulse_min = ULP_MEM_BASE + (14*4)  
+
+        self.ulp = ULP()
+        self.ulp.set_wakeup_period(0, self.sleep_micro_seconds)
+        self.ulp.load_binary(self.load_addr, binary)
 
         mem32[ULP_MEM_BASE + self.load_addr] = 0x0  # initialise state to 0
-        ulp.run(self.entry_addr)
+        # init starting values for wind
+        # min pulse calc.   sleep_micro_seconds * debounce_counter + 1.  5ms * 4 = 20ms
+        mem32[self.ulp_wind_debounce_counter] = 0x1
+        mem32[self.ulp_wind_debounce_max_count] = 0x1
+        mem32[self.ulp_wind_pulse_edge] = 0x1
+        mem32[self.ulp_wind_next_edge] = 0x1
 
-    def _speed(self, count, seconds):
-        cup_r = 80 # 80mm radius of wind cups
-        full_circle = (2 * 3.14 * cup_r)
-        rps = count / seconds
-        # meters per second
-        #mps = ((rps * full_circle) / 1000) 
-        #return mps
-        # temp - just return rps
-        return rps
+        # init starting values for rain
+        mem32[self.ulp_rain_debounce_counter] = 0x1 
+        mem32[self.ulp_rain_debounce_max_count] = 0x1 
+        mem32[self.ulp_rain_pulse_edge] = 0x1  
+        mem32[self.ulp_rain_next_edge] = 0x1
 
-    def windspeed(self, seconds):
-        total_count = mem32[self.wind_counter] & ULP_DATA_MASK
-        gust_count = mem32[self.wind_gust] & ULP_DATA_MASK
-        # clear the counters
-        mem32[self.wind_counter] = 0
-        mem32[self.wind_gust] = 0
+        self.ulp.run(self.entry_addr)
+    
+    def init_rain_holder(self):
+        self.rain_buckets_last_24hours = 0
+        # this is how we determine when 24 hours have rolled over
+        self.rain_bucket_last_reset = time.time()
+        return True
 
-        avg_mps = self._speed(total_count, seconds)
-        gust_mps = self._speed(gust_count, 1)
+    def increment_rain_holder(self, count):
+        # check if we're over 24 hours
+        now = time.time()
+        if self.rain_bucket_last_reset / now > 1:
+            # day rollover
+            self.rain_buckets_last_24hours = count
+            self.rain_bucket_last_rest = now
+        else:
+            self.rain_buckets_last_24hours += count
 
-        return avg_mps, gust_mps
+        return self.rain_buckets_last_24hours
 
-    def rainbuckets(self, reset=None):
-        value = int(mem32[self.rain_counter] & ULP_DATA_MASK)
-        # need to either determine volume of each pulse.
-        if reset is True:
-            mem32[self.rain_counter] = 0
-        return value
+    def retrieve_metrics(self, seconds):
+        dict = {}
+        wind_p, rain_p = self.get_pulse_count()
+        wind_sp, rain_sp = self.get_shortest_pulse(seconds)
+        dict['wind_total_pulse_count'] = wind_p
+        dict['rain_total_pulse_count'] = rain_p
+        dict['wind_avg_pulse_second'] = (wind_p / seconds)
+        dict['rain_avg_pulse_second'] = (rain_p / seconds)
+        # shortest time in seconds betweeen two detected pulses.
+        dict['wind_burst_pulse_second'] = wind_sp 
+        dict['rain_burst_pulse_second'] = rain_sp 
+        #24 hour rain bucket totals
+        dict['rain_total_pulse_count_last_24hour'] = self.increment_rain_holder(rain_p)
+
+        return dict
+
+    def get_pulse_count(self):
+        wind_pulse_count = (mem32[self.ulp_wind_edge_count] & 0xffff) / 2
+        mem32[self.ulp_wind_edge_count] = (mem32[self.ulp_wind_edge_count] & 0xffff) % 2
+        rain_pulse_count = (mem32[self.ulp_rain_edge_count] & 0xffff) / 2
+        mem32[self.ulp_rain_edge_count] = (mem32[self.ulp_rain_edge_count] & 0xffff) % 2
+        return wind_pulse_count, rain_pulse_count
+
+    def get_shortest_pulse(self,seconds):
+        wind_pulse_time_us = (mem32[self.ulp_wind_pulse_min] & 0xffff) * self.sleep_micro_seconds
+        mem32[self.ulp_wind_pulse_min] = 0
+        wind_pulse_time_min = self.compute_shortest_pulse(wind_pulse_time_us, seconds)
+        rain_pulse_time_us = (mem32[self.ulp_rain_pulse_min] & 0xffff) * self.sleep_micro_seconds
+        mem32[self.ulp_rain_pulse_min] = 0
+        rain_pulse_time_min = self.compute_shortest_pulse(rain_pulse_time_us, seconds)
+        return wind_pulse_time_min, rain_pulse_time_min
+
+    def compute_shortest_pulse(self, short_pulse, seconds):
+        if short_pulse == 0:
+            return 0.0
+        # inverse and convert to seconds
+        pulse_per_second = 1 / (short_pulse / 1000000) 
+        if pulse_per_second > seconds:
+            return seconds
+        else:
+            return pulse_per_second
